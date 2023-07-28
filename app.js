6b553c9f954741488e90e71c878670cf9af3e879
andiahmadysx <andiahmadyusup@gmail.com>
2023-07-28 19:23:01 +0700

    fixing bug

--- app.js
+++ app.js
@@ -1,82 +1,83 @@
-const calcAgeBtn = document.getElementById("calc-age-btn");
+window.addEventListener("DOMContentLoaded", () => {
+  const calcAgeBtn = document.getElementById("calc-age-btn");
 
-const ageCalculate = () => {
-  const today = new Date();
-  const inputDate = new Date(document.getElementById("date-input").value);
+  const ageCalculate = () => {
+    const today = new Date();
+    const inputDate = new Date(document.getElementById("date-input").value);
 
-  const birthDetails = {
-    date: inputDate.getDate(),
-    month: inputDate.getMonth(),
-    year: inputDate.getFullYear(),
+    const birthDetails = {
+      date: inputDate.getDate(),
+      month: inputDate.getMonth(),
+      year: inputDate.getFullYear(),
+    };
+
+    const currentDate = {
+      date: today.getDate(),
+      month: today.getMonth(),
+      year: today.getFullYear(),
+    };
+
+    const { years, months, days } = calculateAge(birthDetails, currentDate);
+    if (isFutureDate(years, months, days)) {
+      alert("Not Born Yet");
+      displayResult("-", "-", "-");
+    } else {
+      displayResult(years, months, days);
+    }
   };
 
-  const currentDate = {
-    date: today.getDate(),
-    month: today.getMonth(),
-    year: today.getFullYear(),
+  const isFutureDate = (years, months, days) => {
+    return days < 0 || months < 0 || years < 0;
   };
 
-  const { years, months, days } = calculateAge(birthDetails, currentDate);
-  if (isFutureDate(years, months, days)) {
-    alert("Not Born Yet");
-    displayResult("-", "-", "-");
-  } else {
-    displayResult(years, months, days)
-  }
-};
+  const calculateAge = (birthDetails, currentDate) => {
+    let years = currentDate.year - birthDetails.year;
+    let months, days;
 
-const isFutureDate = (years, months, days) => {
-  return days < 0 || months < 0 || years < 0;
-};
+    if (currentDate.month < birthDetails.month) {
+      years--;
+      months = 12 - (birthDetails.month - currentDate.month);
+    } else {
+      months = currentDate.month - birthDetails.month;
+    }
 
-const calculateAge = (birthDetails, currentDate) => {
-  let years = currentDate.year - birthDetails.year;
-  let months, days;
+    if (currentDate.date < birthDetails.date) {
+      months--;
+      const lastMonth = currentDate.month === 1 ? 12 : currentDate.month - 1;
+      const daysInLastMonth = getDaysInMonth(lastMonth, currentDate.year);
+      days = daysInLastMonth - (birthDetails.date - currentDate.date);
+    } else {
+      days = currentDate.date - birthDetails.date;
+    }
 
-  if (currentDate.month < birthDetails.month) {
-    years--;
-    months = 12 - (birthDetails.month - currentDate.month);
-  } else {
-    months = currentDate.month - birthDetails.month;
-  }
+    return { years, months, days };
+  };
 
-  if (currentDate.date < birthDetails.date) {
-    months--;
-    const lastMonth = currentDate.month === 1 ? 12 : currentDate.month - 1;
-    const daysInLastMonth = getDaysInMonth(lastMonth, currentDate.year);
-    days = daysInLastMonth - (birthDetails.date - currentDate.date);
-  } else {
-    days = currentDate.date - birthDetails.date;
-  }
+  const getDaysInMonth = (month, year) => {
+    const isLeapYear = year % 4 === 0 && (year % 100 != 0 || year % 400 === 0);
+    const getDaysInMonth = [
+      31,
+      isLeapYear ? 29 : 28,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
 
-  return { years, months, days };
-};
+    return getDaysInMonth[month - 1];
+  };
 
-const getDaysInMonth = (month, year) => {
-  const isLeapYear = year % 4 === 0 && (year % 100 != 0 || year % 400 === 0);
-  const getDaysInMonth = [
-    31,
-    isLeapYear ? 29 : 28,
-    31,
-    30,
-    31,
-    30,
-    31,
-    31,
-    30,
-    31,
-    30,
-    31,
-  ];
+  const displayResult = (year, month, days) => {
+    document.getElementById("years").textContent = year;
+    document.getElementById("months").textContent = month;
+    document.getElementById("days").textContent = days;
+  };
 
-  return getDaysInMonth[month - 1];
-};
-
-const displayResult = (year, month, days) => {
-    document.getElementById('years').textContent = year;
-    document.getElementById('months').textContent = month;
-    document.getElementById('days').textContent = days;
-};
-
-calcAgeBtn.addEventListener("click", ageCalculate);
-
+  calcAgeBtn.addEventListener("click", ageCalculate);
+});
